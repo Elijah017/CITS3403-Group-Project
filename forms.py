f657c3b65462fa344b0c01d2776123fbb8688df9
JiayiChengdak <141312407+JiayiChengdak@users.noreply.github.com>
2024-04-15 13:18:01 +0800

    Update forms.py

--- forms.py
+++ forms.py
@@ -7,7 +7,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 class RegisterForm(FlaskForm):
-    username = StringField("Username", validators=[validators.Length(min=3, max=25), validators.DataRequired(message="Please Fill This Field")], render_kw={"placeholder": "Enter your username"})
-    email = StringField("Email", validators=[validators.Email(message="Please enter a valid email address")], render_kw={"placeholder": "Enter your email"})
-    password = PasswordField("Password", validators=[validators.DataRequired(message="Please Fill This Field"), validators.EqualTo(fieldname="confirm", message="Your Passwords Do Not Match")], render_kw={"placeholder": "Enter your password"})
+    username = StringField("Username", validators=[validators.Length(min=3, max=25), validators.DataRequired(message="length should more than 3")], render_kw={"placeholder": "Enter your username"})
+    email = StringField("Email", validators=[validators.Email(message="Invalid email address")], render_kw={"placeholder": "Enter your email"})
+    password = PasswordField("Password", validators=[validators.DataRequired(message="Password length should more than 6"), validators.EqualTo(fieldname="confirm", message="Not Match ")], render_kw={"placeholder": "Enter your password"})
     confirm = PasswordField("Confirm Password", validators=[validators.DataRequired(message="Please Fill This Field")], render_kw={"placeholder": "Confirm your password"})
