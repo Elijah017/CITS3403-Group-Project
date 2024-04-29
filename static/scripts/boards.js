3964119eeb7afa804d4bb6ba7debb8d6a7e73bf6
elijah <elmmullens@gmail.com>
2024-04-29 18:25:45 +0800

    type 1 hiding inactive

--- static/scripts/boards.js
+++ static/scripts/boards.js
@@ -4,8 +4,10 @@ $(function() {
     $(window).change(mod_board_height)
 
     $("tbody tr").each(function() {
-        $(this).click(function() {
-            window.location.href = $(this).attr('href');
+        $(this).click(function(event) {
+            if (event.target.id === "") {
+                window.location.href = $(this).attr('href');
+            }
         })
     })
 });
@@ -13,4 +15,15 @@ $(function() {
 function mod_board_height() {
     let offset = $('#boards-list').offset().top;
     $('#boards-list').css('height', `calc(100% - ${offset}px - 2rem)`);
+}
+
+function restore_board(event, id) {
+    console.log(id);
+}
+
+function delete_board(uri) {
+    $.ajax({
+        url: `${uri}`,
+        method: 'DELETE',
+    })
 }
