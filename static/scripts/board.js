a04db3c626fa7a8b2086b1f43d8b0286037fa3c5
Tommo303 <thomas@oakisland.com.au>
2024-05-08 22:50:30 +0800

    added board.js

---
+++ static/scripts/board.js
@@ -0,0 +1,17 @@
+function dragOver(e) {
+    e.preventDefault();
+  }
+  
+  function dragTask(e) {
+    e.dataTransfer.setData("text", e.target.id);
+  }
+  
+  function dropTask(e) {
+    e.preventDefault();
+    var data = e.dataTransfer.getData("text");
+    e.target.appendChild(document.getElementById(data));
+  }
+
+$(function() {
+   console.log('test');
+});
