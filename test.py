c392b310244f99d5d431b2db98cce197ba407b2c
Edwin Tang <yichentang2003@outlook.com>
2024-05-13 10:53:01 +0800

    Added tests for checkpassword function

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import unittest
 from forms import checkPassword
-from app import create_app, db
+from app import create_app, db, User
 from config import TestConfig
+from wtforms import validators
 
+class Password:
+    def __init__(self, data):
+        self.data = data
 
 # Testing
 class BasicTests(unittest.TestCase):
@@ -11,6 +15,23 @@ class BasicTests(unittest.TestCase):
         self.app_context = testApp.app_context()
         self.app_context.push()
         db.create_all()
+        user1 = User(username="Bus", 
+                        email="123456@i.com", 
+                        password="123")
+        user2 = User(username="Car", 
+                        email="1123ad@a.com", 
+                        password="1234567890")
+        user3 = User(username="Cab", 
+                        email="113dfsd@a.com", 
+                        password="asdjojfajiafa")
+        user4 = User(username="Truck", 
+                        email="11edffsd@a.com", 
+                        password="asfaf143256")
+        db.session.add(user1)
+        db.session.add(user2)
+        db.session.add(user3)
+        db.session.add(user4)
+        db.session.commit()
 
     def tearDown(self):
         db.session.remove()
@@ -18,9 +39,23 @@ class BasicTests(unittest.TestCase):
         self.app_context.pop()
 
     def test_checkPassword(self):
-        s = User.query.get("")
-        self.assertFalse(checkPassword("1234567"))
-        self.assertTrue(checkPassword("Qw12947552?"))
+        p1 = Password(User.query.filter_by(username="Bus").first().password)
+        p2 = Password(User.query.filter_by(username="Car").first().password)
+        p3 = Password(User.query.filter_by(username="Cab").first().password)
+        p4 = Password(User.query.filter_by(username="Truck").first().password)
+        with self.assertRaises(validators.ValidationError) as context:
+            checkPassword(p1)
+        self.assertEqual(context.exception.args[0], "Password length should be more than 10 characters")
+        with self.assertRaises(validators.ValidationError) as context:
+            checkPassword(p2)
+        self.assertEqual(context.exception.args[0], "Password must contain at least one lowercase letter")
+        with self.assertRaises(validators.ValidationError) as context:
+            checkPassword(p3)
+        self.assertEqual(context.exception.args[0], "Password must contain at least one digit")
+        with self.assertRaises(validators.ValidationError) as context:
+            checkPassword(p4)
+        self.assertEqual(context.exception.args[0], "Password must contain at least one uppercase letter")
+        
 
 
 if __name__ == "__main__":
