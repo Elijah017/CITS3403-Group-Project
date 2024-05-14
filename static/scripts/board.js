d12e8b5cd4295f2f69b8d93219f6311c00f9568e
Tommo303 <thomas@oakisland.com.au>
2024-05-14 18:55:18 +0800

    can create a new ticket on a board with a POST request

--- static/scripts/board.js
+++ static/scripts/board.js
@@ -12,6 +12,23 @@ function dragOver(e) {
     e.target.closest(".col-body").appendChild(document.getElementById(data));
   }
 
-$(function() {
-   console.log('test');
+$( document ).ready(() => {
+  $("#newTicketModal").on("submit", (e) => {
+    e.preventDefault();
+    let data = {
+      type: parseInt(e.target.ticketType.value),
+      title: e.target.ticketTitle.value,
+      priority: parseInt(e.target.ticketPriority.value),
+      status: parseInt(e.target.ticketStatus.value),
+      description: e.target.ticketDescription.value 
+    };
+
+    $("#newTicketModal [data-bs-dismiss='modal']").click();
+    e.target.reset();
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("POST", document.URL, false);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(JSON.stringify(data));
+  })
 });
