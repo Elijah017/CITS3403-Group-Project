0a4a0fa2e93912b1ac7ec77344fe86d15260a52a
elijah <elmmullens@gmail.com>
2024-05-18 23:32:12 +0800

    search bar working; trying to get filtering working properly

--- static/scripts/boards.js
+++ static/scripts/boards.js
@@ -1,9 +1,15 @@
+let global = this;
+let map = [];
+
 $(function() {
     mod_board_height();
+
+    $(".search-bar").on("input", search_for_board);
 
     $(window).change(mod_board_height);
 
     $("tbody tr").each(function() {
+        map.push({ name: $(this).find('.boardname-col').text(), id: `#${$(this).attr('id')}` });
         $(this).click(function(event) {
             if (event.target.id === "") {
                 window.location.href = $(this).attr('href');
@@ -13,17 +19,22 @@ $(function() {
 
     $(".dropdown-item").each(function() {
         if ($(this).has("#disp-inact").length === 1) {
-            t = $(this)
             $(this).click(handle_inactive_filter);
+        }
+        else if ($(this).has('#disp-mine').length === 1) {
+            $(this).click(handle_my_boards);
+        }
+        else if ($(this).has('#disp-others').length === 1) {
+            $(this).click(handle_other_boards);
         }
     });
 });
 
 function mod_board_height() {
     let list_offset = $('#boards-list').offset().top;
-    let table_offset = $('.boards-table').offset().bottom;
-    $('#boards-list').css('height', `calc(${list_offset}px - 2rem)`);
-    $('.boards-table tbody').css('max-height', `${table_offset}px`);
+    let table_offset = $('.boards-table').offset().top;
+    $('#boards-list').css('height', `calc(100% - ${list_offset}px - 2rem)`);
+    $('.boards-table tbody').css('height', `calc(${table_offset}px + 33rem)`);
 }
 
 function change_board_state(uri, id) {
@@ -37,14 +48,82 @@ function change_board_state(uri, id) {
             delete: $(`${row} .board-state-col`).text() === "Active"
         }),
         success: function() {
-            location.reload();
+            if (!$("#disp-inact").is(":checked")) { $(row).hide(); }
         },
     });
 }
 
-function handle_inactive_filter(e) {
-    if (e.target.id === "") { e.preventDefault(); }
-    checkbox = $("#disp-inact");
-    checked = !checkbox.is(":checked");
+function handle_other_boards(event) {
+    handle_checkbox(event, '#disp-others');
+    console.log("clicked other boards");
+}
+
+function handle_my_boards(event) {
+    handle_checkbox(event, '#disp-mine');
+    console.log('clicked my boards');
+}
+
+function handle_inactive_filter(event) {
+    handle_checkbox(event, '#disp-inact');
+    for (let row in map) {
+        row = map[row]
+        if (!$("#disp-inact").is(":checked")) { $(row.id).hide(); }
+        else { try_show_row(map[row]); }
+    }
+    console.log("clicked");
+}
+
+function handle_checkbox(event, check_id) {
+    if (event.target.id === "") { event.preventDefault(); }
+    let checkbox = $(check_id);
+    let checked;
+    let target_class = $(event.target).attr('class');
+    if (target_class !== "form-check-input") {
+        checked = !checkbox.is(":checked");
+    } else {
+        checked = checkbox.is(":checked");
+    }
     checkbox.prop('checked', checked);
+}
+
+function search_for_board() {
+    const search_string = $(".search-bar").val().toString();
+    const length = search_string.length;
+
+    for (let row in map) {
+        row = map[row];
+        let comp_str;
+        const name_len = row.name.length;
+
+        if (length < name_len) {
+            comp_str = row.name.slice(0, length);
+        }
+        else if (name_len === length) { comp_str = row.name; }
+        else { comp_str = ""; }
+
+        if (comp_str.toLowerCase() === search_string.toLowerCase()) { try_show_row(row); }
+        else { $(row.id).hide(); }
+    }
+}
+
+function try_show_row(row) {
+    let jqRow = $(row.id);
+    if (!jqRow.is(":hidden")) { return; }
+
+    // get the state of the row
+    let active = jqRow.find(".board-state-col").text();
+    let owner = jqRow.find(".board-owner-col").text();
+
+    // get the state of the filters
+    let factive = !$("#disp-inact").is(":checked");
+    let fmine = $("#disp-mine").is(":checked");
+    let fothers = $("#disp-others").is(":checked");
+
+    let valid = 0;
+    if (owner === "Me" && fmine) { valid = valid | 0b1; }
+    else if (owner !== "Me" && fothers) { valid = valid | 0b1; }
+    if (active === "Active" && factive) { valid = valid | 0b10; }
+    else if (active === "Inactive" && !factive) { valid = valid | 0b10; }
+
+    if (valid === 0b11) { jqRow.css('display', 'table'); }
 }
